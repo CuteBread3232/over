f7073be5ba0e5343a273346cef45ce6948849e86
CuteBread3232 <65818782+CuteBread3232@users.noreply.github.com>
2020-06-25 10:14:20 +0900

    p

--- scripts/h-solar-panel.js
+++ scripts/h-solar-panel.js
@@ -1,4 +1,4 @@
-const HugeSolarPanel = extendContent(SolarGenerator, "huge-solar-panel", {
+const HSolarPanel = extendContent(SolarGenerator, "h-solar-panel", {
 	draw(tile){
 		Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
 		Draw.rect(Core.atlas.find(this.name + "-spinner-0"), tile.drawx() - 0, tile.drawy() + 0, Time.time() + 20);
